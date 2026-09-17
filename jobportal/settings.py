a32ad5e7114321200b530e226db418ef8aa6339b
UdayM07 <alphaytog@gmail.com>
2026-09-17 07:14:23 +0530

    Fix production settings

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -12,8 +12,22 @@ SECRET_KEY = os.getenv(
     "django-insecure-o5a_9kc7^sr&e%&(0ayivl8%jv=zzt!3-2&^e_w469i-9g5_6x",
 )
 
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    "http://127.0.0.1:8000,http://localhost:8000",
+).split(",") 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
+
+
+ALLOWED_HOSTS = [
+    "jobportal-0iwg.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 
@@ -135,3 +149,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':5
 }
+
+
