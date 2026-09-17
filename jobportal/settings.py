42e76c048d9cd45319eda122fb20f15c3f90b0c7
UdayM07 <alphaytog@gmail.com>
2026-09-17 06:17:12 +0530

    Prepare project for deployment

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o5a_9kc7^sr&e%&(0ayivl8%jv=zzt!3-2&^e_w469i-9g5_6x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "core" / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 AUTH_USER_MODEL = "core.User"
